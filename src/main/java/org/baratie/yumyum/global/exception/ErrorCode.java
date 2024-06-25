fa74e0e17b412a221d694a49d88a66faf4151679
seojw4973 <sgw0816@naver.com>
2024-06-25 14:27:24 +0900

    feat : StoreNotFoundException
    가게 id로 값을 찾았을때 존재하지 않는 경우에 대한 에러 처리

--- src/main/java/org/baratie/yumyum/global/exception/ErrorCode.java
+++ src/main/java/org/baratie/yumyum/global/exception/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    ;
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "가게를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
