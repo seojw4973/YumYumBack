d363d3cc7ee3e2266bda4ac7032ed8309a926fea
Cro <junsj1230@naver.com>
2024-06-28 18:56:10 +0900

    feat : 리뷰 수정
    UpdateReviewRequestDto : content와 grade만 수정가능하기 때문에 두 개의 필드만 구성
    
    JPA의 변경감지로 repository.save -> 업데이트

---
+++ src/main/java/org/baratie/yumyum/domain/review/dto/UpdateReviewRequestDto.java
@@ -0,0 +1,11 @@
+package org.baratie.yumyum.domain.review.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateReviewRequestDto {
+
+    private String content;
+    private double grade;
+
+}
