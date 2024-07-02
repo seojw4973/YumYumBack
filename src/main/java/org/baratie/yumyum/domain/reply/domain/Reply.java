6288305665da23070a3fdeac5004e4d787d48c25
seojw4973 <sgw0816@naver.com>
2024-07-02 20:51:48 +0900

    feat : 댓글 작성, 수정, 삭제 기능 구현
    댓글 작성 시 memberId 값을 전달 받기로 api 명세서에 작성했는데, @AuthenticationPrincipal을 통해 member값을 넣어줄 수 있어서 memberId값이 필요한지는 논의 필요

--- src/main/java/org/baratie/yumyum/domain/reply/domain/Reply.java
+++ src/main/java/org/baratie/yumyum/domain/reply/domain/Reply.java
@@ -1,10 +1,13 @@
 package org.baratie.yumyum.domain.reply.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.baratie.yumyum.domain.BaseTimeEntity;
 import org.baratie.yumyum.domain.member.domain.Member;
+import org.baratie.yumyum.domain.reply.dto.UpdateRelyDto;
 import org.baratie.yumyum.domain.review.domain.Review;
+import org.baratie.yumyum.domain.review.dto.UpdateReviewRequestDto;
 
 @Entity
 @Getter
@@ -21,10 +24,25 @@ public class Reply extends BaseTimeEntity {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
     private Review review;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
+
+    /**
+     * 댓글 수정
+     * @param request 변경할 댓글 내용
+     * @return Reply
+     */
+    public Reply updateReview(UpdateRelyDto request) {
+        this.content = request.getContent();
+
+        return this;
+    }
 
 }
