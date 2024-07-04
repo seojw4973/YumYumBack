202ea768412c6e984b9972fdaab3b3ec5a4d9a96
Cro <junsj1230@naver.com>
2024-07-04 18:08:29 +0900

    fix : 즐겨찾기 상태값이 false 로만 넘어오는 에러 해결
    json으로 데이터 받을 때 boolean -> Boolean 변경하여 해결

--- src/main/java/org/baratie/yumyum/domain/favorite/controller/FavoriteController.java
+++ src/main/java/org/baratie/yumyum/domain/favorite/controller/FavoriteController.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.baratie.yumyum.domain.favorite.dto.FavoriteDto;
 import org.baratie.yumyum.domain.favorite.service.FavoriteService;
 import org.baratie.yumyum.domain.member.domain.CustomUserDetails;
+import org.baratie.yumyum.domain.member.domain.Member;
+import org.baratie.yumyum.domain.member.service.MemberService;
+import org.baratie.yumyum.domain.store.domain.Store;
+import org.baratie.yumyum.domain.store.service.StoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,10 +19,16 @@ import org.springframework.web.bind.annotation.*;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+    private final MemberService memberService;
+    private final StoreService storeService;
 
     @PostMapping
     public ResponseEntity<Void> addFavorite(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody FavoriteDto request) {
-        favoriteService.addFavorite(customUserDetails, request);
+
+        Member member = memberService.validationMemberId(customUserDetails.getId());
+        Store store = storeService.validationStoreId(request.getStoreId());
+
+        favoriteService.addFavorite(member, store, request);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
