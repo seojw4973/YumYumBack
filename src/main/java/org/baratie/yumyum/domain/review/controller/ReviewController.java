8d0099a5589b90614b5562a63bc9020d1b4d19d1
Cro <junsj1230@naver.com>
2024-07-05 10:12:11 +0900

    chore : 컨트롤러 메서드 순서 변경
    조회 -> 작성 -> 수정 -> 삭제 순으로 변경

--- src/main/java/org/baratie/yumyum/domain/review/controller/ReviewController.java
+++ src/main/java/org/baratie/yumyum/domain/review/controller/ReviewController.java
@@ -8,6 +8,7 @@ import org.baratie.yumyum.domain.review.dto.ReviewDetailDto;
 import org.baratie.yumyum.domain.review.dto.UpdateReviewRequestDto;
 import org.baratie.yumyum.domain.review.dto.CreateReviewDto;
 import org.baratie.yumyum.domain.review.service.ReviewService;
+import org.baratie.yumyum.domain.store.service.StoreService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -17,50 +18,62 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final StoreService storeService;
 
     /**
-     * 리뷰 작성
-     * @param customUserDetails
-     * @param createReviewDto
-     * @return
+     * 리뷰 전체 조회
      */
-    @PostMapping
-    public ResponseEntity<Void> writeReview(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CreateReviewDto createReviewDto){
-        reviewService.createReview(customUserDetails, createReviewDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @GetMapping("/review")
+    public Slice<ReviewAllDto> getAllReview(@RequestParam("pageNumber") int pageNumber){
+        Pageable pageable = PageRequest.of(pageNumber, 5);
+
+        return reviewService.getAllReview(pageable);
     }
 
-    @GetMapping("/{reviewId}")
+    /**
+     * 리뷰 상세 조회
+     */
+    @GetMapping("/review/{reviewId}")
     public ResponseEntity<ReviewDetailDto> getReviewDetail(@PathVariable Long reviewId) {
         ReviewDetailDto reviewDetail = reviewService.getReviewDetail(reviewId);
 
         return new ResponseEntity<>(reviewDetail, HttpStatus.OK);
     }
 
-    @PatchMapping("/{reviewId}")
+    /**
+     * 리뷰 작성
+     */
+    @PostMapping("/review")
+    public ResponseEntity<Void> writeReview(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CreateReviewDto createReviewDto){
+        reviewService.createReview(customUserDetails, createReviewDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 리뷰 수정
+     */
+    @PatchMapping("/review/{reviewId}")
     public ResponseEntity<Void> updateReview(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long reviewId, @RequestBody UpdateReviewRequestDto request) {
         reviewService.updateReview(customUserDetails.getId(), reviewId, request);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
-    @DeleteMapping("/{reviewId}")
+    /**
+     * 리뷰 삭제
+     */
+    @DeleteMapping("/review/{reviewId}")
     public ResponseEntity<Void> deleteReview(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long reviewId) {
         reviewService.deleteReview(customUserDetails.getId(), reviewId);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @GetMapping
-    public Slice<ReviewAllDto> getAllReview(@RequestParam("pageNumber") int pageNumber){
 
-        Pageable pageable = PageRequest.of(pageNumber, 5);
-        return reviewService.getAllReview(pageable);
-    }
 
 }
--- src/main/java/org/baratie/yumyum/domain/store/controller/StoreController.java
+++ src/main/java/org/baratie/yumyum/domain/store/controller/StoreController.java
@@ -23,28 +23,8 @@ public class StoreController {
     private final StoreService storeService;
 
     /**
-     * 가게 상세 정보 보기
-     * @param store_id
-     * @return
+     * 평점 asc 즐겨찾기 asc top10 맛집
      */
-    @GetMapping("/{storeId}")
-    public ResponseEntity<StoreDetailDto> findStore(@PathVariable("storeId") Long store_id) {
-        StoreDetailDto storeDetailDto = storeService.StoreDetail(store_id);
-        return ResponseEntity.ok(storeDetailDto);
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> registerStore(@RequestBody CreateStoreDto createStoreDto) throws IOException, InterruptedException, ApiException {
-        storeService.createStore(createStoreDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @PatchMapping("/{storeId}")
-    public ResponseEntity<Void> updateStore(@PathVariable Long storeId, @RequestBody UpdateStoreDto request) {
-        storeService.updateStore(storeId, request);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
     @GetMapping("/top10")
     public ResponseEntity<List<MainStoreDto>> findTop10(@RequestParam String local) {
         List<MainStoreDto> Top10List = storeService.getTop10(local);
@@ -52,11 +32,31 @@ public class StoreController {
         return ResponseEntity.status(HttpStatus.OK).body(Top10List);
     }
 
-    @PostMapping("/test")
-    public ResponseEntity<BigDecimal[]> changeAddress(@RequestBody String address) throws IOException, InterruptedException, ApiException {
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(storeService.addressToLagLng(address));
+    /**
+     * 가게 상세 정보
+     */
+    @GetMapping("/{storeId}")
+    public ResponseEntity<StoreDetailDto> findStore(@PathVariable("storeId") Long store_id) {
+        StoreDetailDto storeDetailDto = storeService.StoreDetail(store_id);
+        return ResponseEntity.ok(storeDetailDto);
     }
 
+    /**
+     * 가게 등록
+     */
+    @PostMapping
+    public ResponseEntity<Void> registerStore(@RequestBody CreateStoreDto createStoreDto) throws IOException, InterruptedException, ApiException {
+        storeService.createStore(createStoreDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    /**
+     * 가게 정보 수정
+     */
+    @PatchMapping("/{storeId}")
+    public ResponseEntity<Void> updateStore(@PathVariable Long storeId, @RequestBody UpdateStoreDto request) {
+        storeService.updateStore(storeId, request);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
 }
