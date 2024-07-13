7c61bde7af75d45a338d927bb51817dafac2bfe3
seojw4973 <sgw0816@naver.com>
2024-07-13 14:51:18 +0900

    feat : 이미지 업로드 경로 설정
    s3 경로 Bean 등록

---
+++ src/main/java/org/baratie/yumyum/global/config/BucketConfig.java
@@ -0,0 +1,36 @@
+package org.baratie.yumyum.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BucketConfig {
+
+    @Value("${spring.s3.endpoint}")
+    private String endPoint;
+
+    @Value("${spring.s3.region}")
+    private String region;
+
+    @Value("${spring.s3.accesskey}")
+    private String accessKey;
+
+    @Value("${spring.s3.secretkey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region))
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
--- src/main/java/org/baratie/yumyum/global/utils/file/service/ImageService.java
+++ src/main/java/org/baratie/yumyum/global/utils/file/service/ImageService.java
@@ -1,7 +1,21 @@
 package org.baratie.yumyum.global.utils.file.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
+import org.baratie.yumyum.global.utils.file.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ImageService{
+
+    private final ImageRepository imageRepository;
+    private final AmazonS3 s3;
+
+    @Value("${spring.s3.bucket}")
+    private String bucketName;
+
 }
