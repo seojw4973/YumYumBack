85d5d76b4b3063da5bf5d4c0e94bfc34038b0312
seojw4973 <sgw0816@naver.com>
2024-07-17 10:50:28 +0900

    fix : 로그인 시 유저 Role 정보 리턴

--- src/main/java/org/baratie/yumyum/domain/member/dto/LoginResponseDto.java
+++ src/main/java/org/baratie/yumyum/domain/member/dto/LoginResponseDto.java
@@ -2,6 +2,7 @@ package org.baratie.yumyum.domain.member.dto;
 
 import lombok.*;
 import org.baratie.yumyum.domain.member.domain.Member;
+import org.baratie.yumyum.domain.member.domain.Role;
 
 @Getter
 @Builder
@@ -13,17 +14,7 @@ public class LoginResponseDto {
     String nickName;
     String imageUrl;
     String phoneNumber;
+    Role role;
     String atk;
     String rtk;
-
-//    public LoginResponseDto tranceDto(Member member, String atk, String rtk) {
-//        return LoginResponseDto.builder()
-//                .memberId(member.getId())
-//                .nickName(member.getNickname())
-//                .imageUrl(member.getImageUrl())
-//                .atk(atk)
-//                .rtk(rtk)
-//                .build();
-//
-//    }
 }
