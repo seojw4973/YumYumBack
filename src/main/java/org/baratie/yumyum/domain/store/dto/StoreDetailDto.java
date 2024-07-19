8c1c8eff743723899b6e23f782de042b10404a86
seojw4973 <sgw0816@naver.com>
2024-07-19 16:39:54 +0900

    fix : 맛집 상세 정보 리턴 시 카테고리 컬럼 추가

--- src/main/java/org/baratie/yumyum/domain/store/dto/StoreDetailDto.java
+++ src/main/java/org/baratie/yumyum/domain/store/dto/StoreDetailDto.java
@@ -1,6 +1,7 @@
 package org.baratie.yumyum.domain.store.dto;
 
 import lombok.*;
+import org.baratie.yumyum.domain.category.dto.CategoryDto;
 import org.baratie.yumyum.domain.hashtag.domain.Hashtag;
 import org.baratie.yumyum.domain.menu.domain.Menu;
 
@@ -25,12 +26,13 @@ public class StoreDetailDto {
     private List<String> hashtagList;
     private List<Menu> menuList;
     private List<String> imageList;
+    private List<String> categoryList;
 
     private Long totalReviewCount;
     private Long totalFavoriteCount;
     private Boolean favoriteStatus;
 
-    public StoreDetailDto tranceDto(StoreDetailDto storeDetail, List<String> hashtagList, List<Menu> menuList, List<String> imageList) {
+    public StoreDetailDto tranceDto(StoreDetailDto storeDetail, List<String> hashtagList, List<Menu> menuList, List<String> imageList, List<String> categoryList) {
         StoreDetailDto result = StoreDetailDto.builder()
                 .storeId(storeDetail.getStoreId())
                 .name(storeDetail.getName())
@@ -49,6 +51,7 @@ public class StoreDetailDto {
         result.imageList = imageList;
         result.hashtagList = hashtagList;
         result.menuList = menuList;
+        result.categoryList = categoryList;
 
         return result;
     }
