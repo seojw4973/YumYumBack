5c137d6050fc4bb5cd5557b40ca7bd4ee7ccbe83
seojw4973 <sgw0816@naver.com>
2024-07-23 18:14:29 +0900

    fix : redis config 원복

--- src/main/java/org/baratie/yumyum/global/config/RedisConfig.java
+++ src/main/java/org/baratie/yumyum/global/config/RedisConfig.java
@@ -4,27 +4,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.data.redis.cluster.nodes}")
-    private String[] clusterNodes;
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(Arrays.asList(clusterNodes));
-        return new LettuceConnectionFactory(clusterConfig);
-    }
+    public RedisConnectionFactory redisConnectionFactory() { return new LettuceConnectionFactory(host, port); }
 
     @Primary
     @Bean
